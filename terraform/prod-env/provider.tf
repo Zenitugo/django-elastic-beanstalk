2d5f74ae21041551ae22faf83a31113fd8aee179
zenitugo <ugochiukaegbu19@gmail.com>
2024-06-16 10:07:41 +0100

    providers resource

---
+++ terraform/prod-env/provider.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  profile  = var.profile 
+  region   = var.region
+}
+
+# Configuration for Docker
+provider "docker" {
+  registry_auth {
+    address  = local.aws_ecr_url 
+    username = data.aws_ecr_authorization_token.token.user_name
+    password = data.aws_ecr_authorization_token.token.password
+  }
+  
+}
