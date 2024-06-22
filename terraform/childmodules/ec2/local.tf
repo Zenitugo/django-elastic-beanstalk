c1fb5b855642284e662133e2479f311d0579a706
zenitugo <ugochiukaegbu19@gmail.com>
2024-06-22 20:24:16 +0100

    script used to create instance profile that will determine the configuration of ec2 that elastic beanstalk will use

--- terraform/childmodules/ec2/local.tf
+++
@@ -1,4 +0,0 @@
-# Local variable to determine the instance profile name
-locals {
- ec2_profile_name = length(data.aws_iam_instance_profile.existing.id) == 0 ? aws_iam_instance_profile.iam-role[count.index].name : data.aws_iam_instance_profile.existing.name
-} 
